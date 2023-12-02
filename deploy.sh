36c28156064e7c7e28c7cbcf56dd583b52a8b846
Pavel Mises <id@dqd.cz>
2023-12-02 20:05:56 +0100

    deployment fix

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,5 @@
 
 git pull
 docker compose build
-docker compose run --rm --user root bigfive pdm run ./manage.py collectstatic --noinput --clear
 docker compose -f docker-compose.yml -f docker-compose.prod.yml up -d
 docker image prune -f
